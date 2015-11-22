2a63778ca73a217eb05326d33fd6dd9ee018e016
camerongridley <cgridley@gmail.com>
2015-11-22 11:56:29 -0700

    temporary fix for ChangeStage saving activeView and loading Plan from db instead of user
    -also have "Home" link go to index.jsp for all user types for now until
    I figure out why I am losing the css

--- src/com/cggcoding/controllers/client/tools/ChangeStage.java
+++ src/com/cggcoding/controllers/client/tools/ChangeStage.java
@@ -1,5 +1,7 @@
 package com.cggcoding.controllers.client.tools;
 
+import com.cggcoding.exceptions.DatabaseException;
+import com.cggcoding.exceptions.ValidationException;
 import com.cggcoding.models.Stage;
 import com.cggcoding.models.TreatmentPlan;
 import com.cggcoding.models.User;
@@ -31,20 +33,33 @@ public class ChangeStage extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user = (User)request.getSession().getAttribute("user");
-
-        int treatmentPlanID = Integer.parseInt(request.getParameter("treatmentPlanID"));
-
-        TreatmentPlan treatmentPlan = user.getTreatmentPlan(treatmentPlanID);
-
-        int newViewID = Integer.parseInt(request.getParameter("stageIndex"));
-        treatmentPlan.setActiveViewStageIndex(newViewID);
-        Stage activeStage = treatmentPlan.getActiveViewStage();
-        
-        request.setAttribute("activeStage", activeStage);
-        request.setAttribute("treatmentPlan", treatmentPlan);
-
-        request.getRequestDispatcher("/jsp/client-tools/run-treatment-plan.jsp").forward(request,response);
+		 User user = (User)request.getSession().getAttribute("user");
+	        String forwardTo = "index.jsp";
+	        int treatmentPlanID = Integer.parseInt(request.getParameter("treatmentPlanID"));
+	        
+	        try {
+		        //FIXME - dont get txPlan from user, load from db to prevent concurrency probs
+		        //TreatmentPlan treatmentPlan = user.getTreatmentPlan(treatmentPlanID);
+		        TreatmentPlan treatmentPlan = TreatmentPlan.load(treatmentPlanID);
+		
+		        int newViewID = Integer.parseInt(request.getParameter("stageIndex"));
+		        treatmentPlan.setActiveViewStageIndex(newViewID);
+		        Stage activeStage = treatmentPlan.getActiveViewStage();
+		        
+		        //FIXME change to only update basic info - though I think that is all update() does right now, should change method name to reflect this.
+	        
+				treatmentPlan.update();
+				
+				request.setAttribute("activeStage", activeStage);
+				request.setAttribute("treatmentPlan", treatmentPlan);
+				
+				forwardTo = "/jsp/client-tools/run-treatment-plan.jsp";
+			} catch (ValidationException | DatabaseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	   
+	        request.getRequestDispatcher(forwardTo).forward(request,response);
 
 	}
 
