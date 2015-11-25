aa5307ea6d5c25a9e158fe10110007e36e658a73
camerongridley <cgridley@gmail.com>
2015-11-24 17:36:57 -0700

    session timeout filter updated
    - still not configured properly

--- src/com/cggcoding/filters/SessionTimeout.java
+++ src/com/cggcoding/filters/SessionTimeout.java
@@ -8,43 +8,55 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet Filter implementation class SessionTimeout
  */
-@WebFilter("/WebContent/jsp/*")
+@WebFilter("/WebContent/jsp/")
 public class SessionTimeout implements Filter {
 
     /**
      * Default constructor. 
      */
     public SessionTimeout() {
-        // TODO Auto-generated constructor stub
+       
     }
 
 	/**
 	 * @see Filter#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		
 	}
 
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		
 		System.out.println("Filtered.");
+
+		HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute("user") == null) {
+            response.sendRedirect(request.getContextPath() + "/"); // No logged-in user found, so redirect to login page.
+        } else {
+            chain.doFilter(req, res); // Logged-in user found, so just continue request.
+        }
 		// pass the request along the filter chain
-		chain.doFilter(request, response);
+		chain.doFilter(req, res);
 	}
 
 	/**
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
